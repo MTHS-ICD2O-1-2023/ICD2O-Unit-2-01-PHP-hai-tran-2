e67927bb6421c5511e9bbe99d2ac81721b7be035
Hai Tran <159185497+hai-tran-2@users.noreply.github.com>
2024-03-06 19:06:18 +0000

    added button

--- index.php
+++ index.php
@@ -1,39 +1,48 @@
 <!DOCTYPE html>
-<html lang="en-ca">
-  <head>
-    <meta charset="utf-8" />
-    <meta name="description" content="My first webpage, with JavaScript" />
-    <meta name="keywords" content="mths, ICD2O" />
-    <meta name="author" content="Hai Tran" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="./css/style.css" />
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css" />
-    <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
-    <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
-    <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
-    <link rel="manifest" href="site.webmanifest" />
-    <title>My first webpage, with JavaScript</title>
-  </head>
-  <body>
-    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-    <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-      <header class="mdl-layout__header">
-        <div class="mdl-layout__header-row">
-          <span class="mdl-layout-title">The Hello World program, in PHP</span>
-        </div>
-      </header>
-      <main class="mdl-layout__content">
-        <div class="right-image">
-          <img src="./images/HTML-style-guide.jpeg" alt="study guide">
-        </div>
-        <br />
-        <div class="page-content-php">
-          <div id="answer">
-            <?php echo '<p>Hello, World!</p>'; ?>
-          </div>
-        </div>
-      </main>
-    </div>
-  </body>
+<html>
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="description" content="The Hello World program, in PHP" />
+  <meta name="keywords" content="mths, ICD2O" />
+  <meta name="author" content="Hai Tran" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="./css/style.css" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.light_blue-green.min.css" />
+  <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
+  <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
+  <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
+  <link rel="manifest" href="site.webmanifest" />
+  <title>The Hello World program,in PHP</title>
+</head>
+
+<body>
+  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+  <script>
+    function myButtonClicked() {
+      document.getElementById("hello-world").innerHTML = "<?php echo '<p>Hello, World!</p>'; ?>"
+    }
+  </script>
+  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+    <header class="mdl-layout__header">
+      <div class="mdl-layout__header-row">
+        <span class="mdl-layout-title">The Hello World program, in PHP</span>
+      </div>
+    </header>
+    <main class="mdl-layout__content">
+      <div class="right-image">
+        <img src="./images/HTML-style-guide.jpeg" alt="study guide">
+      </div>
+      <br />
+      <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" onclick="myButtonClicked()">
+        Say Hello, World!
+      </button>
+      <div class="page-content-php">
+        <div id="hello-world"></div>
+      </div>
+    </main>
+  </div>
+</body>
+
 </html>
